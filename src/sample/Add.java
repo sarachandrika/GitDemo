5ec2fb56b85e99f73115b9094c5b6bc63188f0b0
Sarachandrika Panduru <sarachandrika@hotmail.com>
2022-07-22 17:07:56 -0500

    this is my first git project

---
+++ src/sample/Add.java
@@ -0,0 +1,18 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Two numbers: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("The sum is "+(a+b));
+		
+		
+
+	}
+
+}
